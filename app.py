4db3b9412608c6e9ce0fc33be601e574ce16152a
Ahmed Soliman <=>
2019-04-26 14:33:17 +0200

    commit

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = f.Flask(__name__)
 
 @app.route('/')
 def home():
-    return f.render_template('home.html')
+	return f.render_template('home.html')
 
 @app.route('/word_embeddings/<subreddit>')
 def word_embeddings(subreddit=None):
-    return f.render_template('word_embeddings.html',subreddit=subreddit)
+	return f.render_template('word_embeddings.html',subreddit=subreddit)
 
 @app.route('/word_embeddings/<subreddit>',methods=['POST'])
 def word_embeddings(subreddit=None):
 	word = f.request.form['word']
-    return f.render_template('word_embeddings.html',subreddit=subreddit,word=word)
+	return f.render_template('word_embeddings.html',subreddit=subreddit,word=word)
