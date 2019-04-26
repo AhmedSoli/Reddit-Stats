507e9b277a2cc234280216b66ec253e34f77682c
Ahmed Soliman <=>
2019-04-26 21:16:45 +0200

    commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import flask as f
-from word_embeddings import nn
+from word_embeddings import we_nn
+from subreddit_embeddings import se_nn
+
 import pandas as pd
-import io
 
 app = f.Flask(__name__)
 
@@ -14,8 +15,17 @@ def word_embeddings(subreddit=None):
 	return f.render_template('word_embeddings.html',subreddit=subreddit)
 
 @app.route('/word_embeddings/<subreddit>',methods=['POST'])
-def nearest_neighbours(subreddit=None):
-	str_io = io.StringIO()
+def word_embeddings_nn(subreddit=None):
 	word = f.request.form['word']
-	neighbours = nn(word)
+	neighbours = ww_nn(word)
 	return f.render_template('word_embeddings.html',subreddit=subreddit,word=word,neighbours=neighbours)
+
+@app.route('/subreddit_embeddings')
+def subreddit_embeddings():
+	return f.render_template('subreddit_embeddings.html')
+
+@app.route('/subreddit_embeddings',methods=['POST'])
+def subreddit_embeddings_nn():
+	subreddit = f.request.form['subreddit']
+	neighbours = se_nn(subreddit)
+	return f.render_template('subreddit_embeddings.html',subreddit=subreddit,neighbours=neighbours)
---
+++ subreddit_embeddings.py
@@ -0,0 +1,18 @@
+import pickle
+import gzip
+
+
+vectors = pickle.load(gzip.open('subreddit_embeddings/0to500.pickle.gz',"rb"))
+
+def similar_vectors(board,k=100,skip=[]):
+	similarity = {}
+
+	if len(skip) == 0:
+	    skip = [board]
+
+	for subreddit in vectors:
+	    if subreddit not in skip:
+	        similarity[subreddit] = cosine_similarity( \
+	            vectors[board].reshape(1,-1),vectors[subreddit].reshape(1,-1))
+
+	return sorted(similarity.items(),key = lambda x:x[1],reverse=True)[0:k]
