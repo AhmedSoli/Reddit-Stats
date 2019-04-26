56f12a3e411fec892081204e5cb3c96de14a4f4d
Ahmed Soliman <=>
2019-04-26 14:15:47 +0200

    add navbar

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = f.Flask(__name__)
 @app.route('/')
 def home():
     return f.render_template('home.html')
+
+@app.route('/word_embeddings/<subreddit>')
+def word_embeddings():
+    return f.render_template('word_embeddings.html',subreddit=subreddit)
