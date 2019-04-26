f79e65284d0f683830c545c969a07db94fe3e571
Ahmed Soliman <=>
2019-04-26 14:33:37 +0200

    commit

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def word_embeddings(subreddit=None):
 	return f.render_template('word_embeddings.html',subreddit=subreddit)
 
 @app.route('/word_embeddings/<subreddit>',methods=['POST'])
-def word_embeddings(subreddit=None):
+def nearest_neighbours(subreddit=None):
 	word = f.request.form['word']
 	return f.render_template('word_embeddings.html',subreddit=subreddit,word=word)
