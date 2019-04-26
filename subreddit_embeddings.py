d80b619ecd82f570d86ab073087832d5bf8f720b
Ahmed Soliman <=>
2019-04-26 22:05:11 +0200

    commit

--- subreddit_embeddings.py
+++ subreddit_embeddings.py
@@ -6,6 +6,8 @@ vectors = pickle.load(gzip.open('subreddit_embeddings/0to50k.pickle.gz',"rb"))
 boards_sorted = pickle.load(gzip.open('subreddit_embeddings/subreddits_sorted_active_members.pickle.gz',"rb"))
 
 def so_nn(board):
+    if board not in boards_sorted:
+        return [('subreddit does not exist',0)]
     results = []
     idx = (-vectors[board]).argsort()
     i = 0
@@ -19,6 +21,8 @@ def so_nn(board):
     return results
 
 def se_nn(board,k=100,skip=[]):
+    if board not in boards_sorted:
+        return [('subreddit does not exist',0)]
 	similarity = {}
 
 	if len(skip) == 0:
