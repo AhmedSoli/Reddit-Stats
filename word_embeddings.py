152bcfa08833a7d9a0a03c53510ecbbdd1786306
Ahmed Soliman <=>
2019-04-27 02:57:46 +0200

    commit

--- word_embeddings.py
+++ word_embeddings.py
@@ -12,13 +12,13 @@ from gensim.models import FastText as ft
 models = {}
 for subreddit in os.listdir("word_embeddings"):
 	if 'bin' in subreddit:
-		model[subreddit.replace('.bin','')] =  ft.load_fasttext_format('word_embeddings/' + subreddit)
+		models[subreddit.replace('.bin','')] =  ft.load_fasttext_format('word_embeddings/' + subreddit)
 
 def dist(a, b):
     return edit_distance(a, b) / max(len(a), len(b))
 
 
 def we_nn(word,subreddit, min_dist=0, topn=100):
-    candidates = model[subreddit].wv.most_similar(positive=[word], topn=topn)
+    candidates = models[subreddit].wv.most_similar(positive=[word], topn=topn)
     return list(islice(filter(lambda t: dist(word, t[0]) >= min_dist, candidates), None, topn))
 
